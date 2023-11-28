517c46595c0e7fd1dceb56df3e8d9552e562919d
Lawson Buabassah <141939835+lawsonBuabassah@users.noreply.github.com>
2023-11-28 01:36:31 +0000

    lot of changes made

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// Wrap your code in document.ready to ensure that it runs after the DOM is fully loaded
+
 $(document).ready(function () {
 
     var a = 0;
@@ -73,7 +73,42 @@ $(document).ready(function () {
     document.getElementById('button').onclick = function() {
       document.getElementById('modalOverlay').style.display = 'none'
     };
+
+    const carousel = document.querySelector("#carousel > div");
+const totalImages = carousel.children.length;
+let currentIndex = 0;
+
+// Clone the first and last images and append/prepend them to the carousel
+const firstImageClone = carousel.children[0].cloneNode(true);
+const lastImageClone = carousel.children[totalImages - 1].cloneNode(true);
+carousel.appendChild(firstImageClone);
+carousel.insertBefore(lastImageClone, carousel.firstChild);
+
+// Update total images count due to cloning
+const updatedTotalImages = carousel.children.length;
+
+setInterval(() => {
+  // Increase the index to move to the next image
+  currentIndex++;
+
+  // Add transition (smooth slide)
+  carousel.style.transition = 'transform 0.3s ease-out';
+  carousel.style.transform = `translateX(-${currentIndex * (100 / (updatedTotalImages - 2))}%)`;
+
+  // Check if the carousel is showing the cloned first image
+  if (currentIndex === updatedTotalImages - 4) {
+    // Use setTimeout to reset to the real first image instantly
+    setTimeout(() => {
+      // Remove transition to make the jump back to the start invisible
+      carousel.style.transition = 'none';
+      // Reset index and transform to show the real first image
+      currentIndex = 1; // Since the first element is a clone of the last
+      carousel.style.transform = `translateX(-${100 / (updatedTotalImages - 2)}%)`;
+    }, 300); // The delay should be just less than the transition duration
+  }
+}, 2000); // Change to 2000ms (2 seconds) as per your requirement
+
+
+
 };
 });
-
-
